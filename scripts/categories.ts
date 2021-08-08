61e7e7deff6739955b67b10c43f26c2eeac426d7
Mariana Deleau <marianadeleau@gmail.com>
2021-08-08 14:57:51 -0300

    add function categories list

--- scripts/categories.ts
+++ scripts/categories.ts
@@ -1,24 +1,23 @@
-
-
 const formAddCategory = document.getElementById('form-add-category');
+const categoriesList = document.getElementById('categoriesList');
 
 const getIdCategory = () => {
 
-    const storage: LocalStorage = getStorage();
+   const storage: LocalStorage = getStorage();
  
     if(storage.categories.length > 0) {
-       const lastItem = storage.categories[storage.categories.length -1];
-       return lastItem.id + 1;
+      const lastItem = storage.categories[storage.categories.length -1];
+      return lastItem.id + 1;
     } 
  
-    return 1;
+   return 1;
  }
 
  const createCategory = (e) => {
-    e.preventDefault();
+   e.preventDefault();
  
       const form = e.target;
-    
+
       const newCategoryName: string = form.nameCategory.value;
       
       const newCategory: Category = {
@@ -26,13 +25,33 @@ const getIdCategory = () => {
          name: newCategoryName,
          //slug: slugify(newCategoryName)
       }
-     
-    storage.categories.push(newCategory);
+
    
-    localStorage.setItem('key-ahorradas', JSON.stringify(storage));
+   const storageAux = getStorage()
+   storageAux.categories.push(newCategory)
+   localStorage.setItem('key-ahorradas', JSON.stringify(storageAux));
+   
+    addcategoryToList(newCategory.name)
+
+   //storage.categories.push(newCategory);
+   //localStorage.setItem('key-ahorradas', JSON.stringify(storage));
  
  }
  
 formAddCategory.addEventListener('submit', createCategory);
 
+const addcategoryToList = (name) => {
 
+   const newCategoryLine = document.createElement('div');
+   newCategoryLine.innerHTML = `<div class="row mt-5 mb-5">
+   <div class="col-9 align-items-center d-flex">
+      <p class="fs-5">${name}</p>
+   </div>
+   <div class="col-3 d-flex justify-content-end">
+      <button class="btn me-3" type="button"><a class="text-white" href="./editarCategoria.html">Editar</a></button>
+      <button class="btn">Eliminar</button>
+   </div>
+   </div>`;
+   categoriesList.appendChild(newCategoryLine);
+
+}
