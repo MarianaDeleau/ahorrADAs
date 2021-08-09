fa633783cc66bc80b78514573019fae0a93187cb
Yanina Bucca <Yanina@MacBook-Air-de-mac.local>
2021-08-08 21:38:43 -0300

    add select categories in new operation

--- scripts/operations.js
+++ scripts/operations.js
@@ -29,3 +29,19 @@ var addOperation = function (e) {
     localStorage.setItem('key-ahorradas', JSON.stringify(storageAux));
 };
 formOperation.addEventListener("submit", addOperation);
+var categoryNewOperation = function () {
+    var storage = getStorage();
+    var selectCategoriesNuevaOp = document.getElementById("categories-nuevaOp");
+    for (var _i = 0, _a = storage.categories; _i < _a.length; _i++) {
+        var category = _a[_i];
+        // selectCategories.innerHTML += `<option value="${category.id}">${category.name}</option>`
+        var elem = document.createElement("option");
+        elem.innerText = category.name;
+        elem.value = category.id.toString();
+        selectCategoriesNuevaOp.appendChild(elem);
+    }
+};
+var init2 = function () {
+    categoryNewOperation();
+};
+init2();
