62bc198e700ac7b8d49130ebd443d31a0b124e73
Mariana Deleau <marianadeleau@gmail.com>
2021-08-11 17:11:10 -0300

    operation list styles

--- scripts/filters.ts
+++ scripts/filters.ts
@@ -28,7 +28,8 @@ const addOperationToList = (array) => {
     for (const operation of array) {
         const newOperationLine = document.createElement('div');
         newOperationLine.classList.add('row', 'mt-3');
-        newOperationLine.innerHTML = `<div class="col-md-3 d-flex align-items-center ">
+        if (operation.type === 'Gasto') {
+            newOperationLine.innerHTML = `<div class="col-md-3 d-flex align-items-center ">
         <h6>${operation.description}</h6>
         </div>
         <div class="col-md-3 d-flex align-items-center">
@@ -40,12 +41,32 @@ const addOperationToList = (array) => {
         <p class="text-end">${operation.date}</p>
         </div>
         <div class="col-md-2 d-flex align-items-center justify-content-end">
-        <h6 class="text-end">${operation.amount}</h6>
+        <h6 class="text-end" style="color:red; font-weight:700">-${operation.amount}</h6>
         </div>
         <div class="col-md-2 d-flex align-items-end flex-column justify-content-center">
         <a href="" class="text-end">Editar</a>
         <a href="" class="text-end">Eliminar</a>
         </div>`;
+        } else if (operation.type === 'Ganancia') {
+            newOperationLine.innerHTML = `<div class="col-md-3 d-flex align-items-center ">
+        <h6>${operation.description}</h6>
+        </div>
+        <div class="col-md-3 d-flex align-items-center">
+        <div class="badge bg-success p-2 text-white text-wrap" style="width: 6rem;">
+        ${operation.category}
+        </div>
+        </div>
+        <div class="col-md-2 d-flex align-items-center justify-content-end">
+        <p class="text-end">${operation.date}</p>
+        </div>
+        <div class="col-md-2 d-flex align-items-center justify-content-end">
+        <h6 class="text-end" style="color:green; font-weight:700">+${operation.amount}</h6>
+        </div>
+        <div class="col-md-2 d-flex align-items-end flex-column justify-content-center">
+        <a href="" class="text-end">Editar</a>
+        <a href="" class="text-end">Eliminar</a>
+        </div>`;
+        }
         operationsList.appendChild(newOperationLine);
     
     }
