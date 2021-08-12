e641de57a5bb04f683831255044fc9d1b1ec1c3b
Yanina Bucca <Yanina@MacBook-Air-de-mac.local>
2021-08-11 23:24:16 -0300

    add function to remove categories

--- scripts/categories.js
+++ scripts/categories.js
@@ -1,5 +1,5 @@
-var formAddCategory = document.getElementById('form-add-category');
-var categoriesList = document.getElementById('categoriesList');
+var formAddCategory = document.getElementById("form-add-category");
+var categoriesList = document.getElementById("categoriesList");
 var getIdCategory = function () {
     var storage = getStorage();
     if (storage.categories.length > 0) {
@@ -18,23 +18,41 @@ var createCategory = function (e) {
     };
     var storageAux = getStorage();
     storageAux.categories.push(newCategory);
-    localStorage.setItem('key-ahorradas', JSON.stringify(storageAux));
+    localStorage.setItem("key-ahorradas", JSON.stringify(storageAux));
     addcategoryToList();
     //storage.categories.push(newCategory);
     //localStorage.setItem('key-ahorradas', JSON.stringify(storage));
 };
-formAddCategory.addEventListener('submit', createCategory);
+var deleteCategory = function (e) {
+    e.preventDefault();
+    var idToDelete = e.srcElement.dataset.category; // Tenemos el id del elemento a eliminar
+    var storageAux = getStorage(); // Leo el local storage y me lo guardo en esta variable
+    // Recorro el local storage en búsqueda del elemento que tengo que eliminar
+    for (var i = 0; i < storageAux.categories.length; i++) {
+        if (storageAux.categories[i].id == idToDelete) {
+            storageAux.categories.splice(i, 1); // posicion y cuantos elementos,
+            break;
+        }
+    }
+    localStorage.setItem("key-ahorradas", JSON.stringify(storageAux));
+    addcategoryToList();
+};
 var addcategoryToList = function () {
     categoriesList.innerHTML = " ";
     var storage = getStorage();
     for (var _i = 0, _a = storage.categories; _i < _a.length; _i++) {
         var category = _a[_i];
-        var newCategoryLine = document.createElement('div');
-        newCategoryLine.innerHTML = "<div class=\"row mt-5 mb-5\">\n      <div class=\"col-9 align-items-center d-flex\">\n         <p class=\"fs-5\">" + category.name + "</p>\n      </div>\n      <div class=\"col-3 d-flex justify-content-end\">\n         <button class=\"btn me-3\" type=\"button\"><a class=\"text-white\" href=\"./editarCategoria.html\">Editar</a></button>\n         <button class=\"btn\">Eliminar</button>\n      </div>\n      </div>";
+        var newCategoryLine = document.createElement("div");
+        newCategoryLine.innerHTML = "<div class=\"row mt-5 mb-5\">\n      <div class=\"col-9 align-items-center d-flex\">\n         <p class=\"fs-5\">" + category.name + "</p>\n      </div>\n      <div class=\"col-3 d-flex justify-content-end\">\n         <button class=\"btn me-3\" type=\"button\"><a class=\"text-white\" href=\"./editarCategoria.html\">Editar</a></button>\n         <button class=\"btn delete-btn\" data-category=" + category.id + ">Eliminar</button>\n      </div>\n      </div>";
         categoriesList.appendChild(newCategoryLine);
+    }
+    var deleteBtn = document.querySelectorAll(".delete-btn");
+    for (var i = 0; i < deleteBtn.length; i++) {
+        deleteBtn[i].addEventListener("click", deleteCategory);
     }
 };
 var init3 = function () {
     addcategoryToList();
 };
 init3();
+formAddCategory.addEventListener("submit", createCategory);
--- scripts/categories.ts
+++ scripts/categories.ts
@@ -1,71 +1,93 @@
-const formAddCategory = document.getElementById('form-add-category');
-const categoriesList = document.getElementById('categoriesList');
+const formAddCategory = document.getElementById("form-add-category");
+const categoriesList = document.getElementById("categoriesList");
 
 const getIdCategory = () => {
+	const storage: LocalStorage = getStorage();
 
-   const storage: LocalStorage = getStorage();
- 
-    if(storage.categories.length > 0) {
-      const lastItem = storage.categories[storage.categories.length -1];
-      return lastItem.id + 1;
-    } 
- 
-   return 1;
- }
+	if (storage.categories.length > 0) {
+		const lastItem = storage.categories[storage.categories.length - 1];
+		return lastItem.id + 1;
+	}
 
- const createCategory = (e) => {
-   e.preventDefault();
- 
-      const form = e.target;
+	return 1;
+};
 
-      const newCategoryName: string = form.nameCategory.value;
-      
-      const newCategory: Category = {
-         id: getIdCategory(),
-         name: newCategoryName,
-         //slug: slugify(newCategoryName)
-      }
+const createCategory = (e) => {
+	e.preventDefault();
 
-   
-   const storageAux = getStorage()
-   storageAux.categories.push(newCategory)
-   localStorage.setItem('key-ahorradas', JSON.stringify(storageAux));
-   
-   addcategoryToList();
-   //storage.categories.push(newCategory);
-   //localStorage.setItem('key-ahorradas', JSON.stringify(storage));
- 
- }
- 
-formAddCategory.addEventListener('submit', createCategory);
+	const form = e.target;
+
+	const newCategoryName: string = form.nameCategory.value;
+
+	const newCategory: Category = {
+		id: getIdCategory(),
+		name: newCategoryName,
+		//slug: slugify(newCategoryName)
+	};
+
+	const storageAux = getStorage();
+	storageAux.categories.push(newCategory);
+	localStorage.setItem("key-ahorradas", JSON.stringify(storageAux));
+
+	addcategoryToList();
+	//storage.categories.push(newCategory);
+	//localStorage.setItem('key-ahorradas', JSON.stringify(storage));
+};
+
+
+// ##### Eliminar Categoria #####
+
+const deleteCategory = (e) => {
+	e.preventDefault();
+
+	const idToDelete = e.srcElement.dataset.category; //id del elemento a eliminar
+
+	const storageAux = getStorage(); // Leo el local storage y me lo guardo en esta variable
+
+	// Recorro el local storage en búsqueda del elemento que tengo que eliminar
+
+	for (let i = 0; i < storageAux.categories.length; i++) {
+		if (storageAux.categories[i].id == idToDelete) {
+			storageAux.categories.splice(i, 1); // posicion y cuantos elementos elimino
+      break
+		}
+	}
+  
+  localStorage.setItem("key-ahorradas", JSON.stringify(storageAux));
+  addcategoryToList();
+
+};
 
 const addcategoryToList = () => {
+	categoriesList.innerHTML = " ";
+	const storage: LocalStorage = getStorage();
 
-   categoriesList.innerHTML= " "
-   const storage: LocalStorage = getStorage();
-
-   for (const category of storage.categories) {
-      const newCategoryLine = document.createElement('div');
-      newCategoryLine.innerHTML = `<div class="row mt-5 mb-5">
+	for (const category of storage.categories) {
+		const newCategoryLine = document.createElement("div");
+		newCategoryLine.innerHTML = `<div class="row mt-5 mb-5">
       <div class="col-9 align-items-center d-flex">
          <p class="fs-5">${category.name}</p>
       </div>
       <div class="col-3 d-flex justify-content-end">
          <button class="btn me-3" type="button"><a class="text-white" href="./editarCategoria.html">Editar</a></button>
-         <button class="btn">Eliminar</button>
+         <button class="btn delete-btn" data-category=${category.id}>Eliminar</button>
       </div>
       </div>`;
-      categoriesList.appendChild(newCategoryLine);
-      
-   }
-   
 
-}
+		categoriesList.appendChild(newCategoryLine);
+	}
+	const deleteBtn = document.querySelectorAll(".delete-btn");
+	for (let i = 0; i < deleteBtn.length; i++) {
+		deleteBtn[i].addEventListener("click", deleteCategory);
+	}
+};
 
-  
+formAddCategory.addEventListener("submit", createCategory);
+
 const init3 = () => {
-   addcategoryToList();
- 
+	addcategoryToList();
 };
 
 init3();
+
+
