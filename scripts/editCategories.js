3d2e28233c3b6639f4f3757b9fa6036c11fe4b27
Yanina Bucca <Yanina@MacBook-Air-de-mac.local>
2021-08-16 20:26:15 -0300

    edit operations-categories

--- scripts/editCategories.js
+++ scripts/editCategories.js
@@ -7,20 +7,42 @@ var btnEditCategory = document.getElementById("edit-btn2");
 var btnCanceled = document.getElementById("canceled-btn");
 // Busco en la url el id de la categoría que voy a editar.
 var paramsUrl = new URLSearchParams(window.location.search);
-var idUrl = parseInt(paramsUrl.get("id"));
+var idUrl = paramsUrl.get("id");
 var categoryUrl = paramsUrl.get("category");
-inputCategory.value = categoryUrl;
+// inputCategory.value = categoryUrl
+// const editCategories = (e) =>{
+//   e.preventDefault();
+//   const input = inputCategory.value;
+//   if (input != ""){
+//   const storageAux = getStorage();
+//   for (let i = 0; i < storageAux.categories.length; i++) {
+// 		if (storageAux.categories[i].id == idUrl) {
+// 			storageAux.categories[i].name = input;
+// 			break;
+// 		}
+// 	}
+//   localStorage.setItem("key-ahorradas", JSON.stringify(storageAux));
+//   window.location.href = "categorias.html";
+//   }
+// }
 var editCategories = function (e) {
     e.preventDefault();
     var input = inputCategory.value;
     if (input != "") {
         var storageAux = getStorage();
         for (var i = 0; i < storageAux.categories.length; i++) {
-            if (storageAux.categories[i].id == idUrl) {
+            console.log("***storage**", storageAux.categories[i].id.toString(), "***", idUrl);
+            if (storageAux.categories[i].id.toString() == idUrl) {
+                //console.log("***")
                 storageAux.categories[i].name = input;
                 break;
             }
         }
+        storageAux.operations.forEach(function (operation) {
+            if (operation.category.toString() === categoryUrl) {
+                operation.category = input;
+            }
+        });
         localStorage.setItem("key-ahorradas", JSON.stringify(storageAux));
         window.location.href = "categorias.html";
     }
--- scripts/editCategories.ts
+++ scripts/editCategories.ts
@@ -11,26 +11,53 @@ const btnCanceled = document.getElementById("canceled-btn")
 
 // Busco en la url el id de la categoría que voy a editar.
 const paramsUrl = new URLSearchParams(window.location.search);
-const idUrl = parseInt(paramsUrl.get("id"));
+const idUrl = paramsUrl.get("id");
 const categoryUrl = paramsUrl.get("category")
 
-inputCategory.value = categoryUrl
+// inputCategory.value = categoryUrl
 
-const editCategories = (e) =>{
-  e.preventDefault();
-  const input = inputCategory.value;
-  if (input != ""){
-  const storageAux = getStorage();
-  for (let i = 0; i < storageAux.categories.length; i++) {
-		if (storageAux.categories[i].id == idUrl) {
-			storageAux.categories[i].name = input;
-			break;
+// const editCategories = (e) =>{
+//   e.preventDefault();
+//   const input = inputCategory.value;
+//   if (input != ""){
+//   const storageAux = getStorage();
+//   for (let i = 0; i < storageAux.categories.length; i++) {
+// 		if (storageAux.categories[i].id == idUrl) {
+// 			storageAux.categories[i].name = input;
+// 			break;
+// 		}
+// 	}
+//   localStorage.setItem("key-ahorradas", JSON.stringify(storageAux));
+//   window.location.href = "categorias.html";
+//   }
+// }
+
+const editCategories = (e) => {
+	e.preventDefault();
+	const input = inputCategory.value;
+	if (input != "") {
+		const storageAux = getStorage();
+		for (let i = 0; i < storageAux.categories.length; i++) {
+      console.log("***storage**", storageAux.categories[i].id.toString(),"***", idUrl);
+			if (storageAux.categories[i].id.toString() == idUrl) {
+        //console.log("***")
+				storageAux.categories[i].name = input;
+				break;
+			}
 		}
+
+    storageAux.operations.forEach(operation => {
+      if(operation.category.toString() === categoryUrl){
+        operation.category = input;
+      }
+    })
+    
+
+
+		localStorage.setItem("key-ahorradas", JSON.stringify(storageAux));
+		window.location.href = "categorias.html";
 	}
-  localStorage.setItem("key-ahorradas", JSON.stringify(storageAux));
-  window.location.href = "categorias.html";
-  }
-}
+};
 btnEditCategory.addEventListener("click", editCategories);
 
 const canceled = () =>{
