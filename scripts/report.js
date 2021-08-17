5b5632711e95cf0ecaddca86ee56dc08a570f11d
Mariana Deleau <marianadeleau@gmail.com>
2021-08-17 16:44:06 -0300

    report by category and report by month

--- scripts/report.js
+++ scripts/report.js
@@ -1,41 +1,83 @@
 var storage = getStorage();
 var operaciones = storage.operations;
-var totales = {};
+//###### RESUMEN GENERAL ######
+var report = {};
 operaciones.forEach(function (op) {
     var date = new Date(op.date);
-    if (!totales[date.getFullYear()]) {
-        totales[date.getFullYear()] = {};
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    if (!report[year]) {
+        report[year] = {};
     }
-    if (!totales[date.getFullYear()][date.getMonth()]) {
-        totales[date.getFullYear()][date.getMonth()] = {};
+    if (!report[year][month]) {
+        report[year][month] = {};
     }
-    if (!totales[date.getFullYear()][date.getMonth()][op.type]) {
-        totales[date.getFullYear()][date.getMonth()][op.type] = 0;
+    if (!report[year][month][op.category]) {
+        report[year][month][op.category] = {};
     }
-    totales[date.getFullYear()][date.getMonth()][op.type] += Number(op.amount);
+    if (!report[year][month][op.category][op.type]) {
+        report[year][month][op.category][op.type] = 0;
+    }
+    report[year][month][op.category][op.type] += Number(op.amount);
+    return report;
 });
-console.log(totales);
-// let reportByCategory = {};
-// operaciones.forEach((op) => {
-// 	const category = op.category;
-// 	const type = op.type
-// 	if (!reportByCategory[category]) {
-// 		reportByCategory[category] = 0;
-// 	}
-// 	if (!reportByCategory[category]) {
-// 	reportByCategory[category] += Number(op.amount);
-// });
-// console.log(reportByCategory);
+console.log(report);
+//###### RESUMEN POR CATEGORIA ######
 var reportByCategory = {};
 operaciones.forEach(function (op) {
-    //const category = op.category;
-    var category = op.category, type = op.type, amount = op.amount;
-    if (!reportByCategory[category]) {
-        console.log(reportByCategory[category][type]);
-        reportByCategory[category] = 0;
+    if (!reportByCategory[op.category]) {
+        reportByCategory[op.category] = {};
     }
-    if (!reportByCategory[category]) {
-        reportByCategory[category] += Number(amount);
+    if (!reportByCategory[op.category][op.type]) {
+        reportByCategory[op.category][op.type] = 0;
     }
+    reportByCategory[op.category][op.type] += Number(op.amount);
+    return reportByCategory;
 });
 console.log(reportByCategory);
+//###### FUNCION PARA CREAR LAS FILAS CON LOS RESUMENES POR CATEGORIA ######
+// const totalsByCategory = document.getElementById('totalsByCategoryDiv')
+// const createCategoryReportLine = (obj) => {
+// 		for (const op in obj) {
+// 			const categoryTag = createNode('p', { class: "fs-5 badge p-1 text-dark text-wrap ms-3" }, document.createTextNode(DATO));
+// 			const divP = createNode('div', { class: "col-md-3" }, categoryTag);
+// 			const ganancia = createNode('div', { class: "col-md-3 fs-5 text-end text-success" }, document.createTextNode(DATO));
+// 			const gasto = createNode('div', { class: "col-md-3 fs-5 text-end text-danger" }, document.createTextNode(DATO));
+// 			const balance = createNode('div', { class: "col-md-3 fs-5 text-end" }, document.createTextNode(DATO));
+// 			const row = createNode('div', { class: "row mb-4" }, divP, ganancia, gasto, balance);
+// 			totalsByCategory.appendChild(row)
+// 		}
+// }
+// createCategoryReportLine(reportByCategory)
+//###### FUNCION RESUMEN POR MES ######
+var reportByMonth = {};
+operaciones.forEach(function (op) {
+    var date = new Date(op.date);
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    if (!reportByMonth[year]) {
+        reportByMonth[year] = {};
+    }
+    if (!reportByMonth[year][month]) {
+        reportByMonth[year][month] = {};
+    }
+    if (!reportByMonth[year][month][op.type]) {
+        reportByMonth[year][month][op.type] = 0;
+    }
+    reportByMonth[year][month][op.type] += Number(op.amount);
+    return reportByMonth;
+});
+console.log(reportByMonth);
+//###### FUNCION PARA CREAR LAS FILAS CON LOS RESUMENES POR MES ######
+// const totalsByMonth = document.getElementById('totalsByMonthDiv')
+// const createMonthReportLine = (obj) => {
+// 		for (const op in obj) {
+// 			const month = createNode('div', { class: "col-md-3 fs-5 text-start ps-4" }, document.createTextNode(DATO));
+// 			const ganancia = createNode('div', { class: "col-md-3 fs-5 text-end text-success" }, document.createTextNode(DATO));
+// 			const gasto = createNode('div', { class: "col-md-3 fs-5 text-end text-danger" }, document.createTextNode(DATO));
+// 			const balance = createNode('div', { class: "col-md-3 fs-5 text-end" }, document.createTextNode(DATO));
+// 			const row = createNode('div', { class: "row mb-3" }, month, ganancia, gasto, balance);
+// 			totalsByCategory.appendChild(row)
+// 		}
+// }
+// createMonthReportLine(reportByMonth)
--- scripts/report.ts
+++ scripts/report.ts
@@ -2,56 +2,118 @@ const storage: LocalStorage = getStorage();
 
 const operaciones = storage.operations;
 
+//###### RESUMEN GENERAL ######
 
-let totales = {};
+let report = {}
 
 operaciones.forEach((op) => {
 	const date = new Date(op.date);
+	const year = date.getFullYear()
+	const month = date.getMonth()+1
 
-	if (!totales[date.getFullYear()]) {
-		totales[date.getFullYear()] = {};
+	if (!report[year]) {
+		report[year] = {};
 	}
-	if (!totales[date.getFullYear()][date.getMonth()]) {
-		totales[date.getFullYear()][date.getMonth()] = {};
+	if (!report[year][month]) {
+		report[year][month] = {};
 	}
-	if (!totales[date.getFullYear()][date.getMonth()][op.type]) {
-		totales[date.getFullYear()][date.getMonth()][op.type] = 0;
+	if (!report[year][month][op.category]) {
+		report[year][month][op.category] = {};
 	}
-	totales[date.getFullYear()][date.getMonth()][op.type] += Number(op.amount);
+	if (!report[year][month][op.category][op.type]) {
+		report[year][month][op.category][op.type] = 0;
+	}
+
+	report[year][month][op.category][op.type] += Number(op.amount);
+
+	return report
 });
 
-console.log(totales);
+console.log(report);
 
+//###### RESUMEN POR CATEGORIA ######
 
-// let reportByCategory = {};
+let reportByCategory = {}
 
-// operaciones.forEach((op) => {
-// 	const category = op.category;
-// 	const type = op.type
+	operaciones.forEach((op) => {
+		
+		if (!reportByCategory[op.category]) {
+			reportByCategory[op.category] = {};
+		}
 
-// 	if (!reportByCategory[category]) {
-// 		reportByCategory[category] = 0;
-// 	}
-	
-// 	if (!reportByCategory[category]) {
-// 	reportByCategory[category] += Number(op.amount);
-// });
+		if (!reportByCategory[op.category][op.type]) {
+			reportByCategory[op.category][op.type] = 0;
+			
+		}
+		
+		reportByCategory[op.category][op.type] += Number(op.amount);
 
-// console.log(reportByCategory);
+		return reportByCategory
 
-let reportByCategory = {};
+		
+	});		
 
-operaciones.forEach((op) => {
-	//const category = op.category;
-	const { category, type, amount} = op
-	if (!reportByCategory[category]) {
-
-		console.log(reportByCategory[category][type])
-		reportByCategory[category] = 0;
-	}
-	
-	if (!reportByCategory[category]) {
-	reportByCategory[category] += Number(amount);
-});
 
 console.log(reportByCategory);
+
+//###### FUNCION PARA CREAR LAS FILAS CON LOS RESUMENES POR CATEGORIA ######
+
+// const totalsByCategory = document.getElementById('totalsByCategoryDiv')
+// const createCategoryReportLine = (obj) => {
+// 		for (const op in obj) {
+// 			const categoryTag = createNode('p', { class: "fs-5 badge p-1 text-dark text-wrap ms-3" }, document.createTextNode(DATO));
+// 			const divP = createNode('div', { class: "col-md-3" }, categoryTag);
+// 			const ganancia = createNode('div', { class: "col-md-3 fs-5 text-end text-success" }, document.createTextNode(DATO));
+// 			const gasto = createNode('div', { class: "col-md-3 fs-5 text-end text-danger" }, document.createTextNode(DATO));
+// 			const balance = createNode('div', { class: "col-md-3 fs-5 text-end" }, document.createTextNode(DATO));
+// 			const row = createNode('div', { class: "row mb-4" }, divP, ganancia, gasto, balance);
+// 			totalsByCategory.appendChild(row)
+// 		}
+// }
+
+// createCategoryReportLine(reportByCategory)
+
+
+//###### FUNCION RESUMEN POR MES ######
+
+let reportByMonth = {}
+
+operaciones.forEach((op) => {
+	const date = new Date(op.date);
+	const year = date.getFullYear()
+	const month = date.getMonth()+1
+
+	if (!reportByMonth[year]) {
+		reportByMonth[year] = {};
+	}
+	if (!reportByMonth[year][month]) {
+		reportByMonth[year][month] = {};
+	}
+	if (!reportByMonth[year][month][op.type]) {
+		reportByMonth[year][month][op.type] = 0;
+	}
+
+	reportByMonth[year][month][op.type] += Number(op.amount);
+
+	return reportByMonth
+});
+
+console.log(reportByMonth);
+
+
+ //###### FUNCION PARA CREAR LAS FILAS CON LOS RESUMENES POR MES ######
+
+// const totalsByMonth = document.getElementById('totalsByMonthDiv')
+// const createMonthReportLine = (obj) => {
+// 		for (const op in obj) {
+// 			const month = createNode('div', { class: "col-md-3 fs-5 text-start ps-4" }, document.createTextNode(DATO));
+// 			const ganancia = createNode('div', { class: "col-md-3 fs-5 text-end text-success" }, document.createTextNode(DATO));
+// 			const gasto = createNode('div', { class: "col-md-3 fs-5 text-end text-danger" }, document.createTextNode(DATO));
+// 			const balance = createNode('div', { class: "col-md-3 fs-5 text-end" }, document.createTextNode(DATO));
+// 			const row = createNode('div', { class: "row mb-3" }, month, ganancia, gasto, balance);
+// 			totalsByCategory.appendChild(row)
+// 		}
+// }
+
+// createMonthReportLine(reportByMonth)
+
